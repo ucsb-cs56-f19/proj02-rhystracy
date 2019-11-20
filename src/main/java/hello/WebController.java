7a178849a160fa3ba3dd5fccf58cf31572c2912e
Rhys Tracy <rhystracy@cs.ucsb.edu>
2019-11-19 20:16:39 -0800

    rt - added FeatureCollection class to unpack json data

--- src/main/java/hello/WebController.java
+++ src/main/java/hello/WebController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.Map;
 import java.util.HashMap;
 
+import hello.geojson.FeatureCollection
+
 import com.nimbusds.oauth2.sdk.client.ClientReadRequest;
 
 @Controller
@@ -63,6 +65,8 @@ public class WebController {
         model.addAttribute("eqSearch", eqSearch);
         String json = e.getJSON(eqSearch.getDistance(), eqSearch.getMinmag());
         model.addAttribute("json", json);
+	FeatureCollection featureCollection = FeatureCollection.fromJSON(json);
+	model.addAttribute("featureCollection", featureCollection);
 	return "earthquakes/results";
     }
 }
