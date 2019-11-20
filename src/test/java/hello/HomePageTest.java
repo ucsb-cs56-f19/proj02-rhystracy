b3716ae770d99989943b4f334128a3bf36274713
Rhys Tracy <rhystracy@cs.ucsb.edu>
2019-11-19 19:19:13 -0800

    rt - fixed typo in update to homepage test

--- src/test/java/hello/HomePageTest.java
+++ src/test/java/hello/HomePageTest.java
@@ -13,7 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.security.oauth2.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
