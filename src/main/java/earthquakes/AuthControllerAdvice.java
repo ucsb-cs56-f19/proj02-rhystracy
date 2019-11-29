d5b04b59233613e8f7b4f3ac41ac0238b3033f89
Rhys Tracy <rhystracy@cs.ucsb.edu>
2019-11-28 20:45:51 -0800

    rt - renamed package hello to earthquake and made all necessary changes, remade docs directory (deleted to search with grep)

---
+++ src/main/java/earthquakes/AuthControllerAdvice.java
@@ -0,0 +1,72 @@
+package earthquakes;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import earthquakes.repositories.UserRepository;
+import earthquakes.entities.AppUser;
+import java.util.List;
+
+import earthquakes.services.MembershipService;
+
+@ControllerAdvice
+public class AuthControllerAdvice {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired   
+    private MembershipService membershipService;
+
+    @ModelAttribute("isLoggedIn")
+    public boolean getIsLoggedIn(OAuth2AuthenticationToken token){
+        return token != null;
+    }
+
+    @ModelAttribute("id")
+    public String getUid(OAuth2AuthenticationToken token){
+        if (token == null) return "";
+
+        String uid = token.getPrincipal().getAttributes().get("id").toString();
+
+        List<AppUser> users = userRepository.findByUid(uid);
+
+        if (users.size()==0) {
+            AppUser u = new AppUser();
+            u.setUid(uid);
+            u.setLogin(token2login(token));
+            userRepository.save(u);
+        }
+
+        return uid;
+    }
+
+    
+    @ModelAttribute("login")
+    public String getLogin(OAuth2AuthenticationToken token){
+        if (token == null) return "";
+        return token2login(token);
+    }
+
+    @ModelAttribute("isMember")
+    public boolean getIsMember(OAuth2AuthenticationToken token){
+        return membershipService.isMember(token);
+    }
+    @ModelAttribute("isAdmin")
+    public boolean getIsAdmin(OAuth2AuthenticationToken token){
+        return membershipService.isAdmin(token);
+    }
+
+    @ModelAttribute("role")
+    public String getRole(OAuth2AuthenticationToken token){
+        return membershipService.role(token);
+    }
+
+    private String token2login(OAuth2AuthenticationToken token) {
+        return token.getPrincipal().getAttributes().get("login").toString();
+    }
+
+}
---
+++ src/main/java/earthquakes/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package earthquakes.repositories;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import earthquakes.entities.AppUser;
+
+@Repository
+public interface UserRepository extends CrudRepository<AppUser, Long> {
+	List<AppUser> findByUid(String uid);
+}
