b8c4fd61cb04516129e4aa6a4ccccdbbbe10c9b0
Rhys Tracy <rhystracy@cs.ucsb.edu>
2019-12-02 20:43:43 -0800

    rt - added delete controller endpoint

--- src/main/java/earthquakes/controllers/LocationsController.java
+++ src/main/java/earthquakes/controllers/LocationsController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
@@ -66,6 +69,15 @@ public class LocationsController {
       locationRepository.save(location);
       model.addAttribute("locations", locationRepository.findAll());
       return "locations/index";
+    }
+
+    @DeleteMapping("/locations/delete/{id}")
+    public String delete(@PathVariable("id") long id, Model model) {
+	    Location location = locationRepository.findById(id)
+		    .orElseThrow(() -> new IllegalArgumentException("Invalid courseoffering Id:" + id));
+	    locationRepository.delete(location);
+	    model.addAttribute("locations", locationRepository.findAll());
+	    return "locations/index";
     }
 
     @GetMapping("/locations")
